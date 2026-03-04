2e3526512d66966d5626e99d9ef0462a7505d613
huydh-afk <huy.dh.66cntt@ntu.edu.vn>
2026-03-04 21:42:57 +0700

    Tạo file chương trình chính nối với file sinh viên

---
+++ OOP SINH VIEN/src/Chuongtrinhchinh.java
@@ -0,0 +1,14 @@
+
+public class Chuongtrinhchinh {
+
+	public static void main(String[] args) {
+		// Tạo mới 1 sinh viên
+		Sinhvien sv1 = new Sinhvien();
+		// cấp/gán dữ liệu cụ thể
+				sv1.mssv="66123456";
+				sv1.ten="Nguyễn Long";
+				sv1.tuoi=18;
+				sv1.hocBai("Lập trình Java");
+	}
+
+}
