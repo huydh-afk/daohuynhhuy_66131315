8a13155c1e99301247e103a76cda293fbd99b588
huydh-afk <huy.dh.66cntt@ntu.edu.vn>
2026-03-04 21:39:47 +0700

    Nhập dữ liệu và tính BMI in ra

--- Bai2_NhapXuatScanner/src/Baitap2_TinhchisoBMI.java
+++ Bai2_NhapXuatScanner/src/Baitap2_TinhchisoBMI.java
@@ -15,7 +15,20 @@ public class Baitap2_TinhchisoBMI {
         
         double bmi = weight / (height * height);
         
-        
+        System.out.printf("PTn=== KẾT QUẢ ===PTn");
+        System.out.printf("Cân nặng: PT.1f kgPTn", weight);
+        System.out.printf("Chiều cao: PT.2f mPTn", height);
+        System.out.printf("Chỉ số BMI: PT.2fPTn", bmi);
+        // Điều kiện BMI và in ra kết quả
+        if (bmi < 18.5) {
+            System.out.println("Phân loại: Thiếu cân");
+        } else if (bmi < 25) {
+            System.out.println("Phân loại: Bình thường");
+        } else if (bmi < 30) {
+            System.out.println("Phân loại: Thừa cân");
+        } else {
+            System.out.println("Phân loại: Béo phì");
+        }
         
         scanner.close();
 	}
