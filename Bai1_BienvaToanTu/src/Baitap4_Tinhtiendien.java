74b84ca8bd0819fcaa53a1d5ada7dc771a5ca23d
huydh-afk <huy.dh.66cntt@ntu.edu.vn>
2026-03-04 21:36:37 +0700

    Khai báo dữ liệu tiền điện

--- Bai1_BienvaToanTu/src/Baitap4_Tinhtiendien.java
+++ Bai1_BienvaToanTu/src/Baitap4_Tinhtiendien.java
@@ -6,17 +6,7 @@ public class Baitap4_Tinhtiendien {
 		 int soDien = 250;  // kWh
 	     double tienDien = 0;
 	     // Điều kiện và tính tiền điện
-	     if (soDien <= 50) {
-	            tienDien = soDien * 1678;
-	        } else if (soDien <= 100) {
-	            tienDien = 50 * 1678 + (soDien - 50) * 1734;
-	        } else if (soDien <= 200) {
-	            tienDien = 50 * 1678 + 50 * 1734 + (soDien - 100) * 2014;
-	        } else if (soDien <= 300) {
-	            tienDien = 50 * 1678 + 50 * 1734 + 100 * 2014 + (soDien - 200) * 2536;
-	        } else {
-	            tienDien = 50 * 1678 + 50 * 1734 + 100 * 2014 + 100 * 2536 + (soDien - 300) * 2834;
-	        }
+	   
 	     // In kết quả tiền điện thông tin điện
 	     	System.out.println("Số điện: " + soDien + " kWh");
 	        System.out.println("Tiền điện: " + tienDien + " đồng");
