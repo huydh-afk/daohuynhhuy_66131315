643b4fbe707c5d2ed32974962c55f08902e33c0b
huydh-afk <huy.dh.66cntt@ntu.edu.vn>
2026-03-04 21:34:25 +0700

    Tạo file bài 2 chuyển đổi nhiệt độ

---
+++ Bai1_BienvaToanTu/src/Baitap2_Chuyendoinhietdo.java
@@ -0,0 +1,13 @@
+
+public class Baitap2_Chuyendoinhietdo {
+
+	public static void main(String[] args) {
+		// Khai báo biến và công thức chuyển
+		double celsius = 25.0;
+        double fahrenheit = (celsius * 9/5) + 32;
+        // In kết quả chuyển đổi
+        System.out.println(celsius + "°C = " + fahrenheit + "°F");
+
+	}
+
+}
