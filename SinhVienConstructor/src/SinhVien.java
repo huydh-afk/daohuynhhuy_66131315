3b810aff86c5bb080793878f0b20b7e4e2a0cc76
huydh-afk <huy.dh.66cntt@ntu.edu.vn>
2026-03-11 21:53:04 +0700

    Tao ham constructor

--- SinhVienConstructor/src/SinhVien.java
+++ SinhVienConstructor/src/SinhVien.java
@@ -3,4 +3,17 @@ public class SinhVien {
     private int tuoi;
     private String maSV;
     private double diemTB;
+    public SinhVien() {
+        this.ten = "Chưa có tên";
+        this.tuoi = 18;
+        this.maSV = "SV000";
+        this.diemTB = 0.0;
+    }
+    // Constructor có tham số
+    public SinhVien(String ten, int tuoi, String maSV) {
+        this.ten = ten;
+        this.setTuoi(tuoi);  // Sử dụng setter để validate
+        this.maSV = maSV;
+        this.diemTB = 0.0;
+    }
 }
