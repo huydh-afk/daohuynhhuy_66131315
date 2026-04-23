ad80c9bc2698c87da593dc263c61b04490876d48
huydh-afk <huy.dh.66cntt@ntu.edu.vn>
2026-04-23 08:09:58 +0700

    lan2

--- ThiGK2/src/thigk2/daohuynhhuy/SanPham.java
+++ ThiGK2/src/thigk2/daohuynhhuy/SanPham.java
@@ -1,5 +1,29 @@
 package thigk2.daohuynhhuy;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class SanPham {
+    private String maSP;
+    private String tenSP;
+    private String loaiSP;
+    private double giaBan;
+
+    public SanPham(String maSP, String tenSP, String loaiSP, double giaBan) {
+        this.maSP = maSP;
+        this.tenSP = tenSP;
+        this.loaiSP = loaiSP;
+        this.giaBan = giaBan;
+    }
+
+    public String getMaSP() { return maSP; }
+    public void setMaSP(String maSP) { this.maSP = maSP; }
+    public String getTenSP() { return tenSP; }
+    public void setTenSP(String tenSP) { this.tenSP = tenSP; }
+    public String getLoaiSP() { return loaiSP; }
+    public void setLoaiSP(String loaiSP) { this.loaiSP = loaiSP; }
+    public double getGiaBan() { return giaBan; }
+    public void setGiaBan(double giaBan) { this.giaBan = giaBan; }
+
 
 }
